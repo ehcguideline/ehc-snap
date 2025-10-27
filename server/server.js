58ab706670d2c31a3d565c92f437b82ba8db544e
ehcguideline <ehcguideline.info@gmail.com>
2025-10-28 02:40:26 +0600

    Update server.js

--- server/server.js
+++ server/server.js
@@ -24,4 +24,5 @@ app.post("/snap", async (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log("✅ EHC Snap server running on port 3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
