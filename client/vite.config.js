503ee87b5e34f475a8214c29b00d9af45a17f532
ehcguideline <ehcguideline.info@gmail.com>
2025-10-28 03:40:40 +0600

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // ✅ খুব গুরুত্বপূর্ণ লাইন
+  root: './client', //
 })
